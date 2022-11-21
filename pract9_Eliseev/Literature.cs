0d170e0a3337da54a9bc9e0f53b9c81dce0dfc63
danilawarrior <danilawarrior@mail.ru>
2022-11-21 18:11:30 +0300

    Добавьте файлы проекта.

---
+++ pract9_Eliseev/Literature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pract9_Eliseev
+{
+    struct Literature
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }   
+
+        public Literature(string title, string author, string publisher)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+        }
+
+    }
+
+}
---
+++ pract9_Eliseev/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pract9_Eliseev
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Literature> books = new List<Literature>(); //?
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            books.Add(new Literature(titleText.Text, authorText.Text, publisherText.Text));
+            literatureGrid.ItemsSource = null;
+            literatureGrid.ItemsSource = books;
+            
+        }
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            var foundBooks = new StringBuilder();
+            foreach (var item in books)
+            {
+                if (item.Publisher == searchQuery.Text)
+                {
+                    foundBooks.AppendLine(item.Title);
+                }
+            }
+            MessageBox.Show(foundBooks.ToString());
+        }
+
+        private void Remove_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedBook = literatureGrid.SelectedIndex;
+            books.RemoveAt(selectedBook);
+            literatureGrid.ItemsSource = null;
+            literatureGrid.ItemsSource = books;
+        }
+    }
+}
+/// ЗАЩИТА, ОБНОВЛЕНИЕ datagrid
